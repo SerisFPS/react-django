34ed2b8309c1562b43afc3f1f0000fdf58fb9091
seris <serisfps@gmail.com>
2023-10-31 14:55:47 -0700

    set categories - drop down data

--- react/src/components/Admin/Products/AddEditProductsForm/AddEditProductsForm.jsx
+++ react/src/components/Admin/Products/AddEditProductsForm/AddEditProductsForm.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form, Image, Button, Dropdown, Checkbox } from 'semantic-ui-react'
+import { map } from 'lodash'
 import './AddEditProductsForm.scss'
+import { useCategory } from '../../../../hooks/main'
+
 export function AddEditProductsForm() {
+  const { categories, getCategories } = useCategory()
+  const [categoriesFormat, setCategoriesFormat] = useState([])
+
+  useEffect(() => {
+    getCategories()
+  }, [])
+
+  useEffect(() => {
+    setCategoriesFormat(formatDropdownData(categories))
+  }, [categories])
+
   return (
     <Form className="add-edit-product-form">
       <Form.Input name="title" placeholder="product name" />
       <Form.Input type="number" name="price" placeholder="25" />
-      <Dropdown placeholder="product category" fluid selection search />
+      <Dropdown
+        placeholder="product category"
+        fluid
+        selection
+        search
+        options={categoriesFormat}
+      />
       <div className="add-edit-product-form__active">
         <Checkbox toggle />
         Active Product
@@ -19,4 +39,12 @@ export function AddEditProductsForm() {
       <Button type="submit" primary fluid content="create" />
     </Form>
   )
+}
+
+function formatDropdownData(data) {
+  return map(data, (item) => ({
+    key: item.id,
+    text: item.title,
+    value: item.id,
+  }))
 }
